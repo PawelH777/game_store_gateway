9096375ae8098aeb3929abb03be818d41728c933
pawel_hornik <pawel_hornik@epam.com>
2022-11-02 16:38:46 +0100

    Gateway changes

--- src/main/java/org/example/gateway/Application.java
+++ src/main/java/org/example/gateway/Application.java
@@ -1,11 +1,9 @@
-package org.example.zuulproxy;
+package org.example.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@EnableZuulProxy
 @EnableDiscoveryClient
 @SpringBootApplication
 public class Application {
